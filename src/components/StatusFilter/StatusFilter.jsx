96dd4781fa2a6cc3c5b7dcbe3014fe5ade90d1ac
Ваше Ім'я <karffffagen@gmail.com>
2023-08-14 22:12:20 +0300

    finisch write code

---
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -0,0 +1,38 @@
+import { Button } from "../Button/Button";
+import { useSelector, useDispatch } from "react-redux";
+import { setStatusFilter } from "../../redux/actions";
+import { statusFilters } from "../../redux/constants";
+
+export const StatusFilter = () => {
+  // Отримуємо посилання на функцію відправки екшенів
+  const dispatch = useDispatch();
+
+  const filter = useSelector(state => state.statusFilter);
+
+  // Викликаємо генератор екшену та передаємо значення фільтра
+  // Відправляємо результат - екшен зміни фільтра
+  const handleFilterChange = filter => dispatch(setStatusFilter(filter));
+
+  return (
+    <div>
+      <Button
+        selected={filter === statusFilters.all}
+        onClick={() => handleFilterChange(statusFilters.all)}
+      >
+        All
+      </Button>
+      <Button
+        selected={filter === statusFilters.active}
+        onClick={() => handleFilterChange(statusFilters.active)}
+      >
+        Active
+      </Button>
+      <Button
+        selected={filter === statusFilters.completed}
+        onClick={() => handleFilterChange(statusFilters.completed)}
+      >
+        Completed
+      </Button>
+    </div>
+  );
+};
---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,31 @@
+import { useSelector } from "react-redux";
+import { Task } from "../Task/Task";
+import { statusFilters } from "../../redux/constants";
+import css from "./TaskList.module.css";
+
+const getVisibleTasks = (tasks, statusFilter) => {
+  switch (statusFilter) {
+    case statusFilters.active:
+      return tasks.filter(task => !task.completed);
+    case statusFilters.completed:
+      return tasks.filter(task => task.completed);
+    default:
+      return tasks;
+  }
+};
+
+export const TaskList = () => {
+  const tasks = useSelector(state => state.tasks);
+  const statusFilter = useSelector(state => state.filters.status);
+  const visibleTasks = getVisibleTasks(tasks, statusFilter);
+
+  return (
+    <ul>
+      {visibleTasks.map(task => (
+        <li className={css.list} key={task.id}>
+          <Task task={task} />
+        </li>
+      ))}
+    </ul>
+  );
+};
